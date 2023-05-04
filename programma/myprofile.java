03ef82f75104fcd65d2749c5af227357a1b7532d
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2023-05-04 10:53:07 +0800

    add myprofile.java

---
+++ programma/myprofile.java
@@ -0,0 +1,17 @@
+package myprofile;
+
+class Person {
+  String name;
+  int age;
+  String country;
+  public void Person(){
+    name = "ryugen";
+    age = 18;
+    country = "indonesia";
+  }
+  public static String getBio(){
+    return "I'm Ryugen, I enjoy collaborating with friends on Git and Github."+ 
+    "I gain valuable experience by solving issues with bugs and adding new features."+
+    "I prefer working on small features as my abilities aren't as strong as my friends, who help me improve.";
+  }
+}
